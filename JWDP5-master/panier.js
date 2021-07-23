37c25299732bc1ca166af16270c2a8d3dd72328a
HB78 <78313137+HB78@users.noreply.github.com>
2021-07-23 20:03:36 +0200

    creation du menu qui montre le montant total a payer et du bouton clearall pour vider tout le panier

--- JWDP5-master/panier.js
+++ JWDP5-master/panier.js
@@ -1,3 +1,4 @@
+
 //appel à l'API// 
 function getdata() { 
     fetch("http://localhost:3000/api/teddies")
@@ -17,18 +18,17 @@ function getdata() {
 
 //Panier de l'utilisateur
 let panier = JSON.parse(localStorage.getItem("panier"));
+console.log(panier[0].price*panier[0].quantity)
 
 
 
-
-
+//Pour afficher les produits du panier dans le tableau//
 panier.forEach((produitPanier, index) => {
     console.log(typeof produitPanier)
 
         produitPanier.price = produitPanier.quantity * produitPanier.price;
         let tableproduct = document.getElementById("tableproduct")
 
-  for (let produit in produitPanier) {
     tableproduct.innerHTML +=`
       <tr id="info">
         <td>${produitPanier.name}</td>
@@ -36,7 +36,43 @@ panier.forEach((produitPanier, index) => {
         <td>${produitPanier.price}.00 €</td>
       </tr>
     `;
-  } 
+
 })
-console.log(typeof panier)
+
+//calcule le montant total de la facture//
+
+let sommeTotal = 0;
+panier.forEach((produitPanier) => {
+  sommeTotal += produitPanier.price;
+  let facture = document.getElementById('facture')
+  facture.innerHTML = "<div>la facture est de : </div>" + sommeTotal
+  console.log(produitPanier.price)
+});
+
+//le bouton pour supprimer tous les articles//
+
+const btnclear = `
+<button id="clearall"> Vider le panier </button>
+`;
+/*insertion du bouton dans le html du panier*/
+
+tableproduct.insertAdjacentHTML("beforeend", btnclear);
+
+//le clear du bouton//
+const clearall = document.getElementById("clearall")
+console.log(clearall)
+
+//suppression de la key produit du localstorage//
+
+clearall.addEventListener('click', (e) =>{
+  e.preventDefault;
+
+    //on utilise le .removeItem pour vider le local storage//
+    localStorage.removeItem("panier")
+    alert('le panier a été vidé')
+
+    //rechargement de la page avec un panier vide//
+
+    window.location.href = "panier.html"
+})
 
