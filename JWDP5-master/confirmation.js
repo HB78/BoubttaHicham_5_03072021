a2e749e0457b540e6d08b9a6c66c6559f9e4f4b6
HB78 <78313137+HB78@users.noreply.github.com>
2021-08-04 23:15:39 +0200

    rajout des regex aux input du formulaire, retrait du formulaire lorsqu'il n'y a pas de commande, page de confirmation terminée

--- JWDP5-master/confirmation.js
+++ JWDP5-master/confirmation.js
@@ -1,12 +1,23 @@
-let sommeTotal = JSON.parse(localStorage.getItem("sommeTotal"));
-let arraysend = JSON.parse(localStorage.getItem("arraysend"));
-let confirms = document.getElementById("confirm");
-let thanks = `<div>
-<h1>Nous vous remercions pour votre commande ${arraysend.contact.firstName}</h1>
-<p>Votre numéro de commande est le ${arraysend.products[0]}</p>
-<p>Vous avez payé ${sommeTotal}.00€</p>
-<button>Retour à la page d'accuei</button>
-</div>`;
 
-confirms.innerHTML += thanks;
-
+function clearend() {
+    localStorage.removeItem("sommeTotal")
+    localStorage.removeItem("panier")
+    localStorage.removeItem("orderId")
+    localStorage.removeItem("contact")
+    localStorage.removeItem("arraysend")
+    window.location.href = "index.html"
+}
+function principale() {
+    let sommeTotal = JSON.parse(localStorage.getItem("sommeTotal"));
+    orderId = localStorage.getItem("orderId")
+    let arraysend = JSON.parse(localStorage.getItem("arraysend"));
+    let confirms = document.getElementById("confirm");
+    let thanks = `<div>
+    <h1>Nous vous remercions pour votre commande ${arraysend.contact.firstName}</h1>
+    <p>Votre numéro de commande est le ${orderId}</p>
+    <p>Vous avez payé ${sommeTotal}.00€</p>
+    <button id="btnback" onClick = "clearend()">Retour à la page d'accuei</button>
+    </div>`;
+    confirms.innerHTML += thanks;
+}
+principale()
--- JWDP5-master/panier.js
+++ JWDP5-master/panier.js
@@ -1,8 +1,16 @@
 
 /* On récupere le pannier de l'utilisateur dans le local storage */
 let panier = JSON.parse(localStorage.getItem("panier"));
+let form = document.getElementById("form")
 let id = JSON.parse(localStorage.getItem("id"));
 console.log("id", id)
+/*ajout des input du formulaire pour faire fonctionner les regex et la validation des input*/
+let prenom = document.getElementById("firstname")
+let nom = document.getElementById("lastname")
+let lieu = document.getElementById("adress")
+let ville = document.getElementById("city")
+let mail = document.getElementById("mail")
+let phone = document.getElementById("phone")
 
 /*fonction pour envoyer les info avec fetch-post*/
 function postapi() {
@@ -51,6 +59,7 @@ const contact = {
       console.log("fetch Error",err);
     });
 }
+/*un groupe de fonction pour effacer les produits du panier*/
 function delIndexPanier(index) {
   panier.splice(index, 1);
   localStorage.setItem("panier", JSON.stringify(panier));
@@ -62,43 +71,172 @@ function viderPanier() {
   alert('le panier a été vidé');
   window.location.href = "panier.html";
 }
-
-function main() {
-  let sommeTotal = 0;
-  let facture = document.getElementById('facture');
-  let tableproduct = document.getElementById("tableproduct");
-  console.log("panier", panier);
-  /* On remplis le html avec les donné du pannier et on calcule le total*/
-  panier.forEach((produitPanier, index) => {
-    produitPanier.totalPrice = produitPanier.quantity * produitPanier.price;
-    tableproduct.innerHTML += `
-      <tr id="info">
-        <td>${produitPanier.name}</td>
-        <td>${produitPanier.quantity}</td>
-        <td>${produitPanier.price}.00 €</td>
-        <td>${produitPanier.totalPrice}.00 €</td>
-        <td><button id="supprimer" onClick="delIndexPanier(${index})"> <i class="fas fa-trash-alt"></i></button></td>
-      </tr>
-    `;
-    /* On ajoute le prix de l'objet (le prix total) au prix total */
-    sommeTotal += produitPanier.totalPrice;
-    localStorage.setItem("sommeTotal", JSON.stringify(sommeTotal))
-  });
-  facture.innerHTML = "<div class='centerprice' >Le montant total est de : </div>" + sommeTotal + ".00 €";
-
-  /* LA PARTIE FORMULAIRE*/
-    //recuperation du formulaire pour les mettre dans le local storage au clic sur le bouton envoie//
-  
-    const envoieFomulaire = document.getElementById("commander")
-    envoieFomulaire.addEventListener('click', (e)=> {
-      e.preventDefault()
-      postapi()
-  })
+/* DEBUT : un groupe de fonction qui verifie la validité des input du formulaire*/
+function validName(nom){
+  let nameRegex = new RegExp("[a-zA-Z]")
+  let testname =  nameRegex.test(nom.value)
+  let smallname = document.getElementById("smallname");
+  if(testname == true) {
+    smallname.textContent = "ce champs est valide"
+    smallname.style.color = "green"
+    nom.classList.remove('badox')
+    nom.classList.add('goodbox')
+    console.log('c super')
+  }else {
+    console.log("putain")
+    smallname.textContent = "ce champs ne doit contenir des lettres"
+    smallname.style.color = "red"
+    nom.classList.remove('goodbox')
+    nom.classList.add('badbox')
+  }
+}
+function validPrenom(prenom) {
+  let prenomRegex = new RegExp("[a-zA-Z]")
+  let testprenom = prenomRegex.test(prenom.value)
+  let smallprenom = document.getElementById("smallprenom")
+  if(testprenom == true) {
+    console.log('je suis chaud')
+    smallprenom.textContent = "ce champs est valide"
+    smallprenom.style.color = "green"
+    prenom.classList.remove('badox')
+    prenom.classList.add('goodbox')
+  }else {
+    smallprenom.textContent = "ce champs ne doit contenir des lettres"
+    smallprenom.style.color = "red"
+    prenom.classList.remove('goodbox')
+    prenom.classList.add('badbox')
+  }
 }
 
-main();
-// localStorage.removeItem('panier')
-//       window.location.href = "confirmation.html"
-// orderId = data.orderId;
-//       JSON.stringify(orderId)
-//       localStorage.setItem("orderId", orderId);
+function validAdress (lieu) {
+  let adressRegex = new RegExp("[a-zA-Z0-9.-]")
+  let testadress = adressRegex.test(lieu.value)
+  let smalladress = document.getElementById("smalladress")
+  if(testadress == true){
+    smalladress.textContent = "ce champs est valide"
+    smalladress.style.color = "green"
+    lieu.classList.remove('badbox')
+    lieu.classList.add('goodbox')
+  }else {
+    smalladress.textContent = "ce champs ne doit contenir des lettres et des chiffres"
+    smalladress.style.color = "red"
+    lieu.classList.remove('goodbox')
+    lieu.classList.add('badbox')
+  }
+}
+
+function validCity(ville) {
+  let cityRegex = new RegExp("[a-zA-Z]")
+  let testcity = cityRegex.test(ville.value)
+  let smallcity =  document.getElementById("smallcity")
+  if(testcity) {
+    smallcity.textContent = "ce champs est valide"
+    smallcity.style.color = "green"
+    ville.classList.remove('badbox')
+    ville.classList.add('goodbox')
+  }else {
+    smallcity.textContent = "ce champs doit contenir des lettres et des chiffres"
+    smallcity.style.color = "red"
+    ville.classList.remove('goodbox')
+    ville.classList.add('badbox')
+  }
+}
+
+function validEmail(mail) {
+  let mailRegex = new RegExp("^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-zA-Z]{2,10}$")
+  let testmail = mailRegex.test(mail.value)
+  let smallmail = document.getElementById("smallmail")
+  if(testmail) {
+    smallmail.textContent = "ce champs est valide"
+    smallmail.style.color = "green"
+    mail.classList.remove('badbox')
+    mail.classList.add('goodbox')
+  }else {
+    smallcity.textContent = "ce champs est invalide"
+    smallcity.style.color = "red"
+    mail.classList.remove('goodbox')
+    mail.classList.add('badbox')
+  }
+}
+function validPhone(phone) {
+  let phoneRegex = new RegExp("[0-9]")
+  let testphone = phoneRegex.test(phone.value)
+  let smallphone = document.getElementById("smallphone")
+  if(testphone == true) {
+    smallphone.style.color = "green"
+    smallphone.textContent = "ce champs est valide"
+    phone.classList.add("goodbox")
+    phone.classList.remove("badbox")
+  }else {
+    smallphone.textContent = "ce champs ne doit contenir que des chiffres"
+    smallphone.style.color = "red"
+    phone.classList.add("badbox")
+    phone.classList.remove("goodbox")
+  }
+}
+/* FIN : un groupe de fonction qui verifie la validité des input du formulaire*/
+
+if(panier !=null) {
+  function main() {
+    let sommeTotal = 0;
+    let facture = document.getElementById('facture');
+    let tableproduct = document.getElementById("tableproduct");
+    console.log("panier", panier);
+    /* On remplis le html avec les donné du pannier et on calcule le total*/
+    panier.forEach((produitPanier, index) => {
+      produitPanier.totalPrice = produitPanier.quantity * produitPanier.price;
+      tableproduct.innerHTML += `
+        <tr id="info">
+          <td>${produitPanier.name}</td>
+          <td>${produitPanier.quantity}</td>
+          <td>${produitPanier.price}.00 €</td>
+          <td>${produitPanier.totalPrice}.00 €</td>
+          <td><button id="supprimer" onClick="delIndexPanier(${index})"> <i class="fas fa-trash-alt"></i></button></td>
+        </tr>
+      `;
+      /* On ajoute le prix de l'objet (le prix total) au prix total */
+      sommeTotal += produitPanier.totalPrice;
+      localStorage.setItem("sommeTotal", JSON.stringify(sommeTotal))
+    });
+    facture.innerHTML = "<div class='centerprice' >Le montant total est de : </div>" + sommeTotal + ".00 €";
+  
+    /* LA PARTIE FORMULAIRE*/
+    //on test la validité des input avec les regex//
+    nom.addEventListener("blur", () => {
+      validName(nom)
+    })
+    prenom.addEventListener("blur", () => {
+      validPrenom(prenom)
+    })
+    lieu.addEventListener("blur", () => {
+      validAdress(lieu)
+    })
+    ville.addEventListener("blur", () => {
+      validCity(ville)
+    })
+    mail.addEventListener("blur", () => {
+      validEmail(mail)
+    })
+    phone.addEventListener("blur", () => {
+      validPhone(phone)
+    })
+      //recuperation du formulaire pour les mettre dans le local storage au clic sur le bouton envoie//
+        const envoieFomulaire = document.getElementById("commander")
+        envoieFomulaire.addEventListener('click', (e)=> {
+          e.preventDefault()
+          postapi()
+      })
+  }
+  
+  main();
+}else {
+let pan = document.getElementById("panier")
+let livraison = document.getElementById("livraison")
+ form.style.display = "none";
+ livraison.style.display = "none";
+ let attention = `<div>
+<h1>Rajoutez des produits au panier pour pouvoir commander</h1>
+</div>`
+pan.innerHTML += attention;
+pan.style.height = "88vh"
+}
